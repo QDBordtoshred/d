d043d7feb57189217296d3066826adc8cb0d27d1
QDBordtoshred <143145783+QDBordtoshred@users.noreply.github.com>
2024-04-22 12:51:42 -0700

    top

--- assets/js/platformer3x/Tube.js
+++ assets/js/platformer3x/Tube.js
@@ -22,7 +22,7 @@ export class Tube extends GameObject {
         // Formula for Width is scaled: scaledWidth/scaledHeight == this.width/this.height
         const scaledWidth = scaledHeight * this.aspect_ratio;
         const tubeX = .80 * GameEnv.innerWidth;
-        const tubeY = (GameEnv.bottom - scaledHeight);
+        const tubeY = (GameEnv.top - scaledHeight);
 
         // set variables used in Display and Collision algorithms
         this.bottom = tubeY;
